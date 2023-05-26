abe3c79d420adf8837c2ae6d9ba5659c8c7dfe8a
JeroenFrie <j.a.w.frieling@student.tue.nl>
2023-05-26 11:47:39 +0200

    PARTYYY

--- main.py
+++ main.py
@@ -7,6 +7,13 @@ from CSV_Load import CSV_Loader
 
 Molecule_DF = CSV_Loader("tested_molecules-1.csv")
 
+
+index_list = []
+for index in range(len(Molecule_DF)):
+    if Molecule_DF["ALDH1_inhibition"][index] == 0:
+        index_list.append(index)
+Molecule_DF_In = Molecule_DF.drop(index=index_list)
+
 Mol_list = []
 for row in range(len(Molecule_DF)):
     Mol_list.append(Chem.MolFromSmiles(Molecule_DF["SMILES"][row]))
@@ -18,7 +25,6 @@ for row in range(len(Molecule_DF)):
 
 desc_list = [n[0] for n in Descriptors._descList]
 short_desc = [i for i in desc_list if not i.startswith("fr_")]
-
 
 for desc in short_desc:
     temp_list = []
@@ -34,7 +40,7 @@ High_Corr = []
 for row in range(len(Corr_Mol_Des)):
     temp_list = []
     Corr_val = Corr_Mol_Des["ALDH1_inhibition"][row]
-    if Corr_val >= 0.1 and Corr_val != 1:
+    if Corr_val >= 0.1 and Corr_val != 1 or Corr_val <= -0.1 and Corr_val != 1:
         temp_list.append(row)
         temp_list.append(Corr_val)
         High_Corr.append(temp_list)
