4d3f3ce0735aeef4710979983e2d2f9fa820e9dc
bzikken <babette.zikken@gmail.com>
2023-06-01 16:32:57 +0200

    Add 3D descriptors

--- means_3D.py
+++ means_3D.py
@@ -6,7 +6,7 @@ Created on Thu Jun  1 14:37:50 2023
 """
 # for 2D and 3D enzo
 from rdkit import Chem
-from rdkit.Chem import Descriptors, Descriptors3D
+from rdkit.Chem import Descriptors, Descriptors3D, AllChem
 from rdkit.ML.Descriptors import MoleculeDescriptors
 from CSV_Load import CSV_Loader
 from scipy import stats
@@ -14,13 +14,13 @@ import pandas as pd
 import numpy as np
 
 data = CSV_Loader("tested_molecules-1.csv")
-
+num_conformers = 5
 # Create a list of descriptor names
-descriptor_names_3d = ['TPSA', 'Asphericity', 'Eccentricity', 
-                       'InertialShapeFactor', 'NPR2', 'PMI1', 'PMI2', 
-                       'PMI3', 'RadiusOfGyration', 'SpherocityIndex']
-descriptor_names = [desc[0] for desc in Descriptors.descList] + descriptor_names_3d
-
+# descriptor_names_3d = ['TPSA', 'Asphericity', 'Eccentricity', 
+#                        'InertialShapeFactor', 'NPR2', 'PMI1', 'PMI2', 
+#                        'PMI3', 'RadiusOfGyration', 'SpherocityIndex']
+descriptor_names_3d = ['Asphericity']
+descriptor_names = [desc[0] for desc in Descriptors.descList]
 
 # Create a descriptor calculator 
 calc = MoleculeDescriptors.MolecularDescriptorCalculator(descriptor_names)
@@ -29,14 +29,59 @@ non_inhibitor_value = []
 inhibitor_value =[]
 
 for index in range(len(data)):
-    mol = Chem.MolFromSmiles(data["SMILES"][index])
+    mol = Chem.AddHs(Chem.MolFromSmiles(data["SMILES"][index]))
     if data["ALDH1_inhibition"][index] == 0:
+        # Make tuple with all 2d descriptors:
         non_inhib_desc_values= calc.CalcDescriptors(mol)
-        non_inhibitor_value.append(non_inhib_desc_values)
+        
+        # Calculate 3d descriptors:
+        AllChem.EmbedMultipleConfs(mol, num_conformers)
+        driedee_list = []
+        asphericities = []
+        for conf_id in range(num_conformers):
+            conf = mol.GetConformer(conf_id)
+            asphericity = Descriptors3D.Asphericity(mol, confId=conf_id)
+            asphericities.append(asphericity)
+            # Calculate the mean asphericity
+            mean_asphericity = sum(asphericities) / num_conformers
+            driedee_list.append(mean_asphericity)
+            
+            driedee_tuple = tuple(driedee_list)
+        non_inhibitor_value.append(non_inhib_desc_values+driedee_tuple)
     else:
         inhib_desc_values = calc.CalcDescriptors(mol)
-        inhibitor_value.append(inhib_desc_values)
-
+        # Calculate 3d descriptors:
+        AllChem.EmbedMultipleConfs(mol, num_conformers)
+        driedee_list = []
+        asphericities = []
+        for conf_id in range(num_conformers):
+            conf = mol.GetConformer(conf_id)
+            asphericity = Descriptors3D.Asphericity(mol, confId=conf_id)
+            asphericities.append(asphericity)
+            # Calculate the mean asphericity
+            mean_asphericity = sum(asphericities) / num_conformers
+            driedee_list.append(mean_asphericity)
+            
+            driedee_tuple = tuple(driedee_list)
+            inhibitor_value.append(inhib_desc_values+driedee_tuple)
+        
+        
+# for index in range(len(data)):
+#     mol = Chem.MolFromSmiles(data["SMILES"][index])
+#     AllChem.EmbedMultipleConfs(mol, num_conformers)
+#     # Calculate the asphericity for each conformer
+#     asphericities = []
+#     for conf_id in range(num_conformers):
+#         conf = mol.GetConformer(conf_id)
+#         asphericity = Descriptors3D.Asphericity(mol, confId=conf_id)
+#         asphericities.append(asphericity)
+#         # Calculate the mean asphericity
+#         mean_asphericity = sum(asphericities) / num_conformers
+#     if data["ALDH1_inhibition"][index] == 0:
+#         non_inhibitor_value.append(mean_asphericity)
+#     else:
+#         inhibitor_value.append(mean_asphericity)
+        
 mean_non_inhibitors = [sum(col) / len(col) for col in zip(*non_inhibitor_value)]
 mean_inhibitors = [sum(col) / len(col) for col in zip(*inhibitor_value)]
 
@@ -44,7 +89,7 @@ mean_inhibitors = [sum(col) / len(col) for col in zip(*inhibitor_value)]
 mean_ttest_results = []
 significant_count = 0 
 super_significant_count = 0
-for i, descriptor in enumerate(descriptor_names):
+for i, descriptor in enumerate(descriptor_names+descriptor_names_3d):
     non_inhib_values = [desc[i] for desc in non_inhibitor_value]
     inhib_values = [desc[i] for desc in inhibitor_value]
     mean_ttest_result = stats.ttest_ind(non_inhib_values, inhib_values)
