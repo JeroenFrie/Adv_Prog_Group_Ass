03887f3325d3fda24e0772eaf666eae7cc58732e
VivanKennis <V.c.w.kennis@student.tue.nl>
2023-06-08 13:19:40 +0200

    j

--- means_3D.py
+++ means_3D.py
@@ -127,58 +127,48 @@ def Mean_median_desc(filepath):
     median_non_inhibitors_list = [np.median(col) for col in zip(*non_inhibitor_value)]
     median_inhibitors_list = [np.median(col) for col in zip(*inhibitor_value)]
     
-    # Perform t-test per descriptor for means
-    ttest_results_mean = []
-    significant_count_mean = 0 
-    super_significant_count_mean = 0
     
-    # Perform t-test per descriptor for medians
-    ttest_results_median = []
-    significant_count_median = 0
-    super_significant_count_median = 0
+    # Perform t-test per descriptor
+    ttest_results= []
+    significant_count = 0
+    super_significant_count = 0
    
     for i, descriptor in enumerate(descriptor_names_all):
-        mean_non_inhibitors = [desc[i] for desc in non_inhibitor_value]
-        mean_inhibitors = [desc[i] for desc in inhibitor_value]
-        median_non_inhibitors = [desc[i] for desc in non_inhibitor_value]
-        median_inhibitors = [desc[i] for desc in inhibitor_value]
-        
-        ttest_result_mean = stats.ttest_ind(mean_non_inhibitors, mean_inhibitors)
-        ttest_result_median = stats.ttest_ind(median_non_inhibitors, median_inhibitors)
-        
-        ttest_results_mean.append(ttest_result_mean)
-        ttest_results_median.append(ttest_result_median)
-       
-        if ttest_result_mean.pvalue < 0.05:
-            significant_count_mean += 1
-        if ttest_result_mean.pvalue < 0.01:
-            super_significant_count_mean += 1
-        if ttest_result_median.pvalue < 0.05:
-            significant_count_median += 1
-        if ttest_result_median.pvalue < 0.01:
-            super_significant_count_median += 1         
+       non_inhibitor = [desc[i] for desc in non_inhibitor_value]
+       inhib_values = [desc[i] for desc in inhibitor_value]
+       ttest_result = stats.ttest_ind(non_inhibitor, inhib_values)
+       ttest_results.append(ttest_result)
+       if ttest_result.pvalue < 0.05:
+           significant_count += 1
+       if ttest_result.pvalue < 0.01:
+           super_significant_count += 1       
     
     # Create a DataFrame with the mean, median, and descriptor names
     df = pd.DataFrame({'Descriptor': descriptor_names_all,
-                       'mean_non_inhibitors': mean_non_inhibitors_list,
-                       'mean_inhibitors': mean_inhibitors_list,
-                       'median_non_inhibitors': median_non_inhibitors_list,
-                       'median_inhibitors': median_inhibitors_list,
-                       'T-Statistic Mean': [result.statistic for result in ttest_results_mean],
-                       'p-value Mean': [result.pvalue for result in ttest_results_mean],
-                       'T-Statistic Median': [result.statistic for result in ttest_results_median],
-                       'p-value Median': [result.pvalue for result in ttest_results_median]})
-    
+                     'mean_non_inhibitors': mean_non_inhibitors_list,
+                     'mean_inhibitors': mean_inhibitors_list,
+                     'median_non_inhibitors': median_non_inhibitors_list,
+                     'median_inhibitors': median_inhibitors_list,
+                     'T-Statistic': [result.statistic for result in ttest_results],
+                     'p-value': [result.pvalue for result in ttest_results],
+                      })
     # Add significance columns
-    df['Super_Significance Mean'] = df['p-value Mean'].apply(lambda p: 'woooooooooow' if p < 0.01 else 'nope, deze niet')
-    df['p-value < 0.01 Count'] = super_significant_count_mean
-    df['Significance Median'] = df['p-value Median'].apply(lambda p: 'wooooooow' if p < 0.01 else 'nope, deze niet')
-    df['p-value < 0.01 Count'] = super_significant_count_median
+    df['Significance'] = df['p-value'].apply(
+        lambda p: 'goed verschilletje hiero' if p < 0.05 else 'nope, deze niet')
+    df['p-value < 0.05 Count'] = significant_count
+    df['Super_Significance'] = df['p-value'].apply(
+        lambda p: 'woow' if p < 0.01 else 'nope')
+    df['p-value < 0.01 Count'] = super_significant_count
     
     # Lijstjes voor Jeroen:
     super_significant_descriptors_mean_list = df[df['p-value Mean'] < 0.01]['Descriptor'].tolist()
     super_significant_descriptors_median_list = df[df['p-value Median'] < 0.01]['Descriptor'].tolist()
     
-    df.to_csv('means_table_3D.csv', index=False)
+    df.to_csv('means_median_stat_table_3D.csv', index=False)
+    
+    # make second table with only the descriptors with significant difference 
+    df2 = df [df['p-value']<0.01]
+    df2 = df2[['Descriptor', 'mean_non_inhibitors','mean_inhibitors', 'median_non_inhibitors', 'median_inhibitors', 'p-value',]]
+    df2.to_csv('descriptors_low_Pvalue_3D.csv', index=False)
 
 Mean_median_desc('tested_molecules-1.csv')
